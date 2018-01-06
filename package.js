ad0ff7dc3d1d02e5cfa3f3e903735c06408276ad
Jan Hendrik Mangold <jhm@cotren.net>
2018-01-06 09:19:14 -0800

    bump package version

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'jamgold:bootstrap3boilerplate',
   summary: 'Provide basic bootstrap-3 template with alert & modal & navbar & footer',
-  version: '0.0.4_5',
+  version: '0.0.4_6',
   git: 'https://github.com/jamgold/jamgold-bootstrap3boilerplate'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['reactive-var','jamgold:isdevelopment@0.0.1','ecmascript']);
+  api.use(['reactive-var','jamgold:isdevelopment@0.0.1','ecmascript@0.9.0']);
   api.use(['templating','handlebars'], 'client');
   api.export('Bootstrap3boilerplate');
   api.addAssets('public/development.png','client');
